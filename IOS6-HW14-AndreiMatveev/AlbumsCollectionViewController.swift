2fabf0a28745736c9c2456f2864f56ae7641127c
AndreiMatveev <andreimatveev55@gmail.com>
2022-06-07 22:02:14 +0700

    addAndSettingNavigationController

--- IOS6-HW14-AndreiMatveev/AlbumsCollectionViewController.swift
+++ IOS6-HW14-AndreiMatveev/AlbumsCollectionViewController.swift
@@ -11,8 +11,32 @@ class AlbumsCollectionViewController: UICollectionViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        collectionView.backgroundColor = .gray
+        setupCollectionView()
+        setupNavigationBar()
+        collectionView.backgroundColor = .systemBackground
+    }
+    
+    private func setupCollectionView() {
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellId")
+    }
+    
+    private func setupNavigationBar() {
+        let addButton = UIButton()
+        addButton.setImage(UIImage(systemName: "plus"), for: .normal)
+        navigationItem.leftBarButtonItem = UIBarButtonItem.init(customView: addButton)
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.largeTitleDisplayMode = .automatic
+        title = "Альбомы"
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 90
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
+        cell.backgroundColor = .green
+        return cell
     }
     
 }
