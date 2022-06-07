d641d920ad837f6922b0ec0eec2d990d9918b696
AndreiMatveev <andreimatveev55@gmail.com>
2022-06-07 17:49:30 +0700

    add albums collection view

--- IOS6-HW14-AndreiMatveev/AppDelegate.swift
+++ IOS6-HW14-AndreiMatveev/AppDelegate.swift
@@ -15,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         let tapBarController = UITabBarController()
+        tapBarController.tabBar.backgroundColor = .systemBackground
         
         let firstViewController = UIViewController()
         firstViewController.tabBarItem = UITabBarItem(title: "Медиатека", image: UIImage(systemName: "photo.on.rectangle"), tag: 0)
@@ -22,7 +23,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let secondViewController = UIViewController()
         secondViewController.tabBarItem = UITabBarItem(title: "Для вас", image: UIImage(systemName: "heart.text.square"), tag: 1)
         
-        let thirdViewController = UIViewController()
+        let thirdViewController = AlbumsCollectionViewController(collectionViewLayout: UICollectionViewFlowLayout())
         thirdViewController.tabBarItem = UITabBarItem(title: "Альбомы", image: UIImage(systemName: "rectangle.stack.fill"), tag: 2)
         
         let fourViewController = UIViewController()
